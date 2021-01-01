a0dafcf1662b6fa42893af91fc6398e28397fe4a
Keymer Jimenez <keimeralexanderj@gmail.com>
2021-01-01 11:01:23 -0500

    se creo la ventana modal para generar los anuncios

--- src/components/GeekMain/Posts.jsx
+++ src/components/GeekMain/Posts.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './../../styles/posts.css'
-import desingAnuncio from './../../img/desingAnuncio.JPG'
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function Posts() {
+
+class Posts extends React.Component {
+    state={
+        abierto: false,
+    }
+
+    abrirModal=()=>{
+        this.setState({abierto: !this.state.abierto})
+    }
+    render (){
     return (
         //revisar el tamaño del panel central que esta en el estilo de dashboard.css el cual tiene como id cont-anuncios -- lo defini con un tamaño de 80%
         <div className="border border-primary">
@@ -13,40 +23,33 @@ function Posts() {
             <br />
             <div className="border"><i className="fas fa-bell"><h1>Anuncios</h1></i></div> {/*  preguntar al profesor por los iconos */}
             <br />
-            <div><button type="button" className="btn btn-warning">Generar anuncio</button></div>
-            <br/>
-            {/* <div id="contenedor-de-publicaciones" className="border"> */}
-            {/* preguntar al profe cuando uno crea un formulario se envia lo que esta dentro del formulario mas no lo de sus contenedor padres que en mi casi es el div con id="generador-anuncios" */}
-            {/* <div className="border" id="generador-anuncios">
-                <form action="" className="form-group" id="form-anuncio">
-                    <div id="text-area-buton">
-                        <textarea className="form-control" id="area-publicar-anuncio" rows="4" placeholder="Publique su anuncio..."  />
-                        <button id="boton-publicar-anuncion" type="submit" className="btn btn-primary">Publicar</button>
-                    </div>
-                </form>
-            </div> */}
-            {/* estructuracion de la tabla de Anuncios */}
-            {/* <div>
-            <table className="border " id="tabla-anuncios">
-                <tbody className="border ">
-                    <tr className="border ">
-                        <img className="img-fluid" src={desingAnuncio} alt="" />
-                    </tr>
-                    <br />
-                    <tr className="border ">
-                        <img className="img-fluid" src={desingAnuncio} alt="" />
-                    </tr>
-                    <br />
-                    <tr className="border ">
-                        <img className="img-fluid" src={desingAnuncio} alt="" />
-                    </tr>
-                </tbody>
-            </table> 
-            </div> */}
-            
-            {/* </div> */}
+            {/* <!-- Button trigger modal --> */}
+            <Button color="warning"  onClick={this.abrirModal} className="font-weight-bold">
+                Generar Anuncio
+            </Button>
+
+            <Modal isOpen={this.state.abierto}>
+                <ModalHeader>
+                    Anuncios Geek
+                </ModalHeader>
+                <ModalBody id="">
+                    <form action="">
+                        <textarea name="" id="" cols="63" rows="8" placeholder="Escriba su anuncio...">
+
+                        </textarea>
+                        
+                    </form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary">Publicar</Button>
+                    <Button color="danger" onClick={this.abrirModal}>Cerrar</Button>
+                </ModalFooter>
+            </Modal>
+
         </div>
+           
     )
+    }
 }
 
 export default Posts
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,30 @@
+{/* <div id="contenedor-de-publicaciones" className="border"> */}
+            {/* preguntar al profe cuando uno crea un formulario se envia lo que esta dentro del formulario mas no lo de sus contenedor padres que en mi casi es el div con id="generador-anuncios" */}
+            {/* <div className="border" id="generador-anuncios">
+                <form action="" className="form-group" id="form-anuncio">
+                    <div id="text-area-buton">
+                        <textarea className="form-control" id="area-publicar-anuncio" rows="4" placeholder="Publique su anuncio..."  />
+                        <button id="boton-publicar-anuncion" type="submit" className="btn btn-primary">Publicar</button>
+                    </div>
+                </form>
+            </div> */}
+            {/* estructuracion de la tabla de Anuncios */}
+            {/* <div>
+            <table className="border " id="tabla-anuncios">
+                <tbody className="border ">
+                    <tr className="border ">
+                        <img className="img-fluid" src={desingAnuncio} alt="" />
+                    </tr>
+                    <br />
+                    <tr className="border ">
+                        <img className="img-fluid" src={desingAnuncio} alt="" />
+                    </tr>
+                    <br />
+                    <tr className="border ">
+                        <img className="img-fluid" src={desingAnuncio} alt="" />
+                    </tr>
+                </tbody>
+            </table> 
+            </div> */}
+
+            {/* </div> */}
