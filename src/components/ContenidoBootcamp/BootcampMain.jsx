58ad5244bb5af5c9cdcc2108f0c58fc72c750afe
Keymer Jimenez <keimeralexanderj@gmail.com>
2021-01-01 12:41:22 -0500

    se creo el menu de navegacion correspondientes a los retos de formacion

--- src/components/ContenidoBootcamp/BootcampMain.jsx
+++ src/components/ContenidoBootcamp/BootcampMain.jsx
@@ -4,20 +4,61 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function BootcampMain() {
     return (
-        <div className="container-fluid border">
-            <div>
-                <section>
-                    <ul>
-                        <li><span><a href="">RETOS DE FORMACIÓN</a></span></li>
-                    </ul>
-                </section>
-                <section>
-                    
+        <div className="container-fluid">
+            <div className="row border">
+            <section className="col-" id="menu-lateral-de-tareas">
+                <ul>
+                    <li><span><a href="">RETOS DE FORMACIÓN</a></span>
+                        <ul>
+                            <li><span><a href="">Entregas FCC  Web 1-2</a></span>
+                                <ul>
+                                    <li><span><a href="">Página Tributo</a></span></li>
+                                    <li><span><a href="">Formulario de encuesta</a></span></li>
+                                </ul>
+                            </li>
+                            <li><span><a href="">Entregas FCC  Web 3-4-5</a></span>
+                                <ul>
+                                    <li><span><a href="">Página de destino del producto</a></span></li>
+                                    <li><span><a href="">Página de documentación técnica</a></span></li>
+                                    <li><span><a href="">Página web portafolio personal</a></span></li>
+                                </ul>
+                            </li>
+                            <li><span><a href="">Entrega GitBook Web</a></span>
+                                <ul>
+                                    <li><span><a href="">Retos Introdución a la Web</a></span></li>
+                                    <li><span><a href="">Retos Fundamentos de programacion Parte I</a></span></li>
+                                    <li><span><a href="">Retos Fundamentos de programacion Parte II</a></span></li>
+                                </ul>
+                            </li>
+                            <li><span><a href="">Entregas FCC JavaScript</a></span>
+                                <ul>
+                                    <li><span><a href="">Comprobador de palindrome</a></span></li>
+                                    <li><span><a href="">Convertidor de números romanos</a></span></li>
+                                    <li><span><a href="">Cifrado de Caesars</a></span></li>
+                                    <li><span><a href="">Validador de números de teléfonos</a></span></li>
+                                    <li><span><a href="">Caja Registradora</a></span></li>
+                                </ul>
+                            </li>
+                            <li><span><a href="">Entregas FCC Bibliotecas Front-end</a></span>
+                                <ul>
+                                    <li><span><a href="">Maquína de cotizaciones al azar</a></span></li>
+                                    <li><span><a href="">Visor de Markdown</a></span></li>
+                                    <li><span><a href="">Caja de Ritmos</a></span></li>
+                                    <li><span><a href="">Calculadora de JavaScript</a></span></li>
+                                    <li><span><a href="">Reloj 25 + 5</a></span></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </section>
+            <section className="col- border" id="contenedor-de-evidencias">
 
-                </section>
-            </div>
-            
+
+            </section>
         </div>
+        </div>
+        
     )
 }
 
