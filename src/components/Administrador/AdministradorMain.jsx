ce92e5a3bdc912678fa879ae6fc4e8d7f0b679e7
Keymer Jimenez <keimeralexanderj@gmail.com>
2021-01-27 16:58:27 -0500

    se creo el panel de control para que los administradores hagan seguimiento a los alumnos

--- src/components/Administrador/AdministradorMain.jsx
+++ src/components/Administrador/AdministradorMain.jsx
@@ -1,12 +1,24 @@
 import React, {Component} from 'react'
 import HeaderInstructores from './HeaderInstructores'
+import './administrador.css'
+import EstadoBootcamp from './EstadoBootcamp'
+
 
 class AdministradorMain extends Component {
     render(){
     return (
         <div>
-            <HeaderInstructores></HeaderInstructores>
-            <div className="border border-warning"></div>
+            <header>
+                <HeaderInstructores></HeaderInstructores>
+            </header>
+            <body id="body">
+                <div className="border border-success" id="panel-izquierdo-administrador">
+                    panel izquierdo
+                </div>
+                <div className="border border-warning" id="panel-central-administrador">
+                    <EstadoBootcamp></EstadoBootcamp>
+                </div>
+            </body>
         </div>
     )
     }
